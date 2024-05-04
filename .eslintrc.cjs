61fe5110bee59d7bb9aee82a289030d8e9003d5a
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-04 17:32:16 +0300

    fix: fix ESLint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,39 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'airbnb',
-    'airbnb-typescript/base',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript/base',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'commitlint.config.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: '**/tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    "react/function-component-definition": "off",
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
     'prettier/prettier': 'error',
   },
 };
