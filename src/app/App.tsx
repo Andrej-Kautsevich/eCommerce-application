f9243a148e9ccab836660c49db5849f5bb628f08
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-09 11:40:29 +0300

    feat: add button to test api request

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import apiRoot from '../api/apiRoot';
+
 const App = () => {
-  return <div> </div>;
+  const [dataDetails, setDataDetails] = useState({});
+
+  const getData = async () => {
+    try {
+      const data = await apiRoot.get().execute();
+
+      setDataDetails(data.body);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData().catch((error: Error) => error);
+  }, []);
+
+  return (
+    <div>
+      <button type="button" onClick={() => console.log(JSON.stringify(dataDetails, undefined, 2))}>
+        click!
+      </button>
+    </div>
+  );
 };
 
 export default App;
