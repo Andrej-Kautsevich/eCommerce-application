86a8191f11adfdf1420d24da669862fc17e5b268
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-10 14:48:56 +0300

    feat: add plugin react refresh, add index file to auth

---
+++ src/api/auth/index.ts
@@ -0,0 +1,5 @@
+import userLogin from './userLogin';
+import userLogout from './userLogout';
+import userSignUp from './userSignUp';
+
+export { userLogin, userLogout, userSignUp };
--- src/api/auth/userLogin.ts
+++ src/api/auth/userLogin.ts
@@ -1,8 +1,8 @@
 import { MyCustomerSignin } from '@commercetools/platform-sdk';
 import apiClient from '../ApiClient';
 
-const userAuth = (user: MyCustomerSignin) => {
+const userLogin = (user: MyCustomerSignin) => {
   return apiClient.apiRoot.me().login().post({ body: user }).execute();
 };
 
-export default userAuth;
+export default userLogin;
