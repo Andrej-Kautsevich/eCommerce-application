54a6bd2887eb4953883316198ea59152759f5203
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-11 13:52:13 +0300

    refactor: removed tests buttons in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,48 +1,5 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-import { MyCustomerSignin } from '@commercetools/platform-sdk';
-import { useEffect } from 'react';
-import useApiClient from '../api/useApiClient';
-import { useCustomer, useCustomerAuth } from '../api/hooks';
-
 const App = () => {
-  const user: MyCustomerSignin = { email: 'example@example.com', password: 'example' };
-
-  const { apiRoot, setAnonymousFlow, setPasswordFlow } = useApiClient();
-
-  useEffect(() => {
-    if (!apiRoot) {
-      setAnonymousFlow();
-    }
-  }, [apiRoot, setAnonymousFlow]);
-
-  const { customerLogin, customerLogOut } = useCustomerAuth();
-  const { getCustomer } = useCustomer();
-
-  const login = async () => {
-    await customerLogin(user).then(() => setPasswordFlow(user));
-  };
-
-  const getUser = async () => {
-    await getCustomer();
-  };
-
-  const logOut = async () => {
-    customerLogOut();
-  };
-
-  return (
-    <div>
-      <button type="button" onClick={login}>
-        click!
-      </button>
-      <button type="button" onClick={getUser}>
-        getCart!
-      </button>
-      <button type="button" onClick={logOut}>
-        logOut!
-      </button>
-    </div>
-  );
+  return <div> </div>;
 };
 
 export default App;
