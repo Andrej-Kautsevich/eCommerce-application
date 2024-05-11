ef5a6b1d60be98b99a99b87f094e5aea97f00468
Andrii <andriyshopyak@gmail.com>
2024-05-11 12:46:44 +0300

    feat: add show/hide password functionality

--- src/components/login/loginTab.tsx
+++ src/components/login/loginTab.tsx
@@ -9,22 +9,29 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import schema from '../passValidation/passValidation';
+import { useState } from 'react';
+import schema from '../passValidation/passValidation';
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function SignIn() {
+  const [content, setContent] = useState('');
+  const [testContent, setTestContent] = useState('password');
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // const value = {
-    //   email: data.get('email'),
-    //   password: data.get('password'),
-    // };
-    // schema
-    //   .validate(value)
-    //   .then((valid) => console.log(valid))
-    //   .catch((error) => alert(error));
+    const data = new FormData(event.currentTarget);
+    const value = {
+      email: data.get('email'),
+      password: data.get('password'),
+    };
+    schema
+      .validate(value)
+      .then(() => setContent(''))
+      .catch((error) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        setContent(error.message);
+      });
   };
 
   return (
@@ -68,11 +75,27 @@ export default function SignIn() {
                 fullWidth
                 name="password"
                 label="Password"
-                type="password"
+                type={testContent}
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+              <h4 style={{ color: 'red', fontWeight: 300 }}>{content}</h4>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onClick={() => {
+                      if (testContent === 'password') {
+                        setTestContent('text');
+                      } else {
+                        setTestContent('password');
+                      }
+                    }}
+                    value="remember"
+                    color="primary"
+                  />
+                }
+                label="Show Password"
+              />
               <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                 Sign In
               </Button>
