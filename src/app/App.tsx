468425c5ab3d7c81d16f23368acbd2c2557a7b0e
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-16 00:52:03 +0300

    feat: add authorization flows creation in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import './reset.css';
 import './App.css';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 import AppRoutes from '../components/AppRoutes';
 import AuthContext from '../components/context';
+import tokenCache from '../shared/utils/tokenCache';
+import { RootState } from '../shared/store';
+import { useApiClient } from '../api/hooks';
 
 const theme = createTheme({
   palette: {
@@ -24,6 +28,22 @@ const App = () => {
     }),
     [isAuth, setIsAuth],
   );
+
+  // get Auth status from local storage
+  const isAuthCustomer = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const { apiRoot, setAnonymousFlow, setTokenFlow } = useApiClient();
+  useEffect(() => {
+    if (!apiRoot) {
+      const refreshToken = tokenCache.get().token;
+      // if user has authorization
+      // TODO handle anonymous refresh token
+      if (isAuthCustomer && refreshToken) {
+        setTokenFlow(refreshToken);
+      } else {
+        setAnonymousFlow();
+      }
+    }
+  }, [apiRoot, setAnonymousFlow, setTokenFlow, isAuthCustomer]);
 
   return (
     <ThemeProvider theme={theme}>
