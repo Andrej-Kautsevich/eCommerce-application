d7e4ffa6173d61c1d93b0813a6bf59d1a49cd30b
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-16 22:17:15 +0300

    refactor: refactor password element  with react-hook-form-mui

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,25 +1,20 @@
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useState } from 'react';
 import { DevTool } from '@hookform/devtools';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { PasswordElement, TextFieldElement } from 'react-hook-form-mui';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { TextFieldElement } from 'react-hook-form-mui';
 import schemaEmail from '../validation/emailValidation';
 import schemaPass from '../validation/passValidation';
 import Header from '../Header';
 
 export default function LoginTab() {
-  const [passwordVisibility, setPasswordVisibility] = useState('password');
-
   const schema = yup.object().shape({
     email: schemaEmail,
     password: schemaPass,
@@ -38,9 +33,6 @@ export default function LoginTab() {
 
   return (
     <div className="top-panel">
-      {/* <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-          Create Account
-        </Button> */}
       <Header />
       <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -71,32 +63,15 @@ export default function LoginTab() {
               autoFocus
               control={control}
             />
-            <TextFieldElement
+            <PasswordElement
               margin="normal"
               required
               fullWidth
               name="password"
               label="Password"
-              type={passwordVisibility}
               id="password"
               autoComplete="current-password"
               control={control}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  onClick={() => {
-                    if (passwordVisibility === 'password') {
-                      setPasswordVisibility('text');
-                    } else {
-                      setPasswordVisibility('password');
-                    }
-                  }}
-                  value="remember"
-                  color="primary"
-                />
-              }
-              label="Show Password"
             />
             <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 3, mb: 2 }}>
               Sign In
