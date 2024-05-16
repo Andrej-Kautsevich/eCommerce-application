a3c844fe8fb6450bf8882517ed2ad8bb50ecd05b
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-16 22:05:20 +0300

    refactor: refactor with react-hook-form-mui

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,7 +1,6 @@
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
@@ -10,30 +9,16 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useState } from 'react';
 import { DevTool } from '@hookform/devtools';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { TextFieldElement } from 'react-hook-form-mui';
 import schemaEmail from '../validation/emailValidation';
 import schemaPass from '../validation/passValidation';
 import Header from '../Header';
 
 export default function LoginTab() {
   const [passwordVisibility, setPasswordVisibility] = useState('password');
-  // const [errors, setErrors] = useState({
-  //   email: '',
-  //   password: '',
-  // });
-  // const schemas = {
-  //   email: schemaEmail,
-  //   password: schemaPass,
-  // };
-  // type SchemaKeys = 'email' | 'password';
-  // function validateField(name: SchemaKeys, value: string) {
-  //   schemas[name]
-  //     .validate(value)
-  //     .then(() => setErrors((prev) => ({ ...prev, [name]: '' })))
-  //     .catch((error: Error) => setErrors((prev) => ({ ...prev, [name]: error.message })));
-  // }
 
   const schema = yup.object().shape({
     email: schemaEmail,
@@ -45,11 +30,7 @@ export default function LoginTab() {
     password: string;
   };
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginForm>({ mode: 'onChange', resolver: yupResolver(schema) });
+  const { control, handleSubmit } = useForm<LoginForm>({ mode: 'onChange', resolver: yupResolver(schema) });
   const onSubmit: SubmitHandler<LoginForm> = (data, event) => {
     event?.preventDefault();
     console.log(data);
@@ -79,54 +60,28 @@ export default function LoginTab() {
           </Typography>
           {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
           <Box component="form" onSubmit={handleSubmit(onSubmit)} style={{ width: '320px' }} noValidate sx={{ mt: 1 }}>
-            <Controller
+            <TextFieldElement
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
               name="email"
+              autoComplete="email"
+              autoFocus
               control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  // eslint-disable-next-line react/jsx-props-no-spreading
-                  {...field}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-              )}
             />
-            {errors.email && (
-              <h4 style={{ fontFamily: 'Lato', fontWeight: 300, fontSize: '15px', color: 'red' }}>
-                {errors.email.message}
-              </h4>
-            )}
-            <Controller
+            <TextFieldElement
+              margin="normal"
+              required
+              fullWidth
               name="password"
+              label="Password"
+              type={passwordVisibility}
+              id="password"
+              autoComplete="current-password"
               control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  // eslint-disable-next-line react/jsx-props-no-spreading
-                  {...field}
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type={passwordVisibility}
-                  id="password"
-                  autoComplete="current-password"
-                />
-              )}
             />
-            {errors.password && (
-              <h4 style={{ fontFamily: 'Lato', fontWeight: 300, fontSize: '15px', color: 'red' }}>
-                {errors.password.message}
-              </h4>
-            )}
             <FormControlLabel
               control={
                 <Checkbox
