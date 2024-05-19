8536f8a6e9eef8274e7b9b18610d55575955f9ab
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-19 18:06:20 +0300

    fix: fix app test render

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-rational-order'],
   validate: ['css', 'scss'],
+  ignoreFiles: ['coverage/**/*.css'],
   overrides: [
     {
       files: ['**/*.scss'],
---
+++ src/test/test-render.tsx
@@ -0,0 +1,23 @@
+import { RenderOptions, RenderResult, render } from '@testing-library/react';
+import { FC, ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import store from '../shared/store';
+import ApiClientProvider from '../api/ApiClientProvider';
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const customRender = (ui: React.ReactElement, { ...renderOptions }: RenderOptions = {}): RenderResult => {
+  const Wrapper: FC<WrapperProps> = ({ children }) => {
+    return (
+      <Provider store={store}>
+        <ApiClientProvider>{children}</ApiClientProvider>
+      </Provider>
+    );
+  };
+
+  return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
+
+export default customRender;
