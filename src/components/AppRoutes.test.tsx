8c582b20fb1cb79e46ad62f4411b5e7e00d22f73
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-22 20:16:43 +0300

    Feature/app tests (#32)
    * feat: add test coverage
    
    * fix: fix app test render
    
    * chore: update dependencies
    
    * test: add routing tests

---
+++ src/components/AppRoutes.test.tsx
@@ -0,0 +1,75 @@
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { expect, it } from 'vitest';
+import AppRoutes from './AppRoutes';
+import { RoutePaths } from '../shared/types/enum';
+import customRender from '../test/test-render';
+
+it('handle routing between pages', async () => {
+  customRender(
+    <MemoryRouter initialEntries={[RoutePaths.MAIN]}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+
+  const loginLink = screen.getByRole('button', { name: 'Login' });
+
+  await waitFor(() => fireEvent.click(loginLink));
+
+  expect(screen.getByText('Sign in')).toBeInTheDocument();
+});
+
+it('display error page', async () => {
+  customRender(
+    <MemoryRouter initialEntries={['/error']}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+
+  expect(screen.getByText('Oops, Something Went Wrong')).toBeInTheDocument();
+});
+
+it('renders basket page without crashing', () => {
+  const { container } = customRender(
+    <MemoryRouter initialEntries={[RoutePaths.BASKET]}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+  expect(container.firstChild).toBeInTheDocument();
+});
+
+it('renders error404 page without crashing', () => {
+  const { container } = customRender(
+    <MemoryRouter initialEntries={['/error']}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+  expect(container.firstChild).toBeInTheDocument();
+});
+
+it('renders login page without crashing', () => {
+  const { container } = customRender(
+    <MemoryRouter initialEntries={[RoutePaths.LOGIN]}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+  expect(container.firstChild).toBeInTheDocument();
+});
+
+it('renders main page without crashing', () => {
+  const { container } = customRender(
+    <MemoryRouter initialEntries={[RoutePaths.MAIN]}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+  expect(container.firstChild).toBeInTheDocument();
+});
+
+it('renders registration page without crashing', () => {
+  const { container } = customRender(
+    <MemoryRouter initialEntries={[RoutePaths.REGISTRATION]}>
+      <AppRoutes />
+    </MemoryRouter>,
+  );
+  expect(container.firstChild).toBeInTheDocument();
+});
