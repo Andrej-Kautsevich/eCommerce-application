7834fbf34afc38ac1ae10e9b3b4c56621c745e8e
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-25 22:21:42 +0300

    feat: add HeaderLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Link } from '@mui/material';
 import { ShoppingBasket } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import LoginLogoutButtons from './LoginLogoutButtons';
 import useBasketButton from './useBasketButton';
+import HeaderLink from '../../shared/ui/HeaderLink';
 
 const Header = () => {
   const basketButton = useBasketButton();
@@ -11,8 +12,12 @@ const Header = () => {
     <AppBar position="static" sx={{ width: '100vw', pr: 8, pl: 8 }} color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} fontFamily="Orbitron">
-          <Link to="/">Volcano Watch</Link>
+          <RouterLink to="/">Volcano Watch</RouterLink>
         </Typography>
+        <HeaderLink underline="none">Watches</HeaderLink>
+        <Link component={RouterLink} to="/">
+          Watches
+        </Link>
 
         <LoginLogoutButtons />
 
