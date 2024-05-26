beb9729718a64b7ba77bee6c3e2a1141b505a43e
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-26 17:37:09 +0300

    feat: add Responsive App bar with Drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,51 @@
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography, MenuItem, Box, Drawer } from '@mui/material';
 import { ShoppingBasket } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 import LoginLogoutButtons from './LoginLogoutButtons';
 import useBasketButton from './useBasketButton';
 
 const Header = () => {
   const basketButton = useBasketButton();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prevState) => !prevState);
+  };
 
   return (
-    <AppBar position="static" sx={{ width: '100vw', pr: 8, pl: 8 }} color="secondary">
+    <AppBar position="static" sx={{ boxShadow: 0 }} color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} fontFamily="Orbitron">
           <Link to="/">Volcano Watch</Link>
         </Typography>
 
-        <LoginLogoutButtons />
+        {/* Mobile view */}
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            onClick={handleDrawerToggle}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="right" open={drawerOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }}>
+            <Box sx={{ minWidth: '60dvw', p: 2, flexDirection: 'column' }}>
+              <MenuItem>
+                <LoginLogoutButtons />
+              </MenuItem>
+            </Box>
+          </Drawer>
+        </Box>
+
+        {/* Desktop view */}
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <LoginLogoutButtons />
+        </Box>
 
         <IconButton color="inherit" onClick={() => basketButton()}>
           <ShoppingBasket color="primary" />
