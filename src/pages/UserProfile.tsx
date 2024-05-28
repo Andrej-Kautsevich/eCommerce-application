83670041aeb911923a8a59469ff7c133f63f0829
Andrii Shopiak <andriyshopyak@gmail.com>
2024-05-28 10:12:43 +0300

    refactor: fix code commented into pull

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,12 @@
 import UserProfile from '../components/UserProfile';
+import MainLayout from '../shared/ui/MainLayout';
 
 const UserProfilePage = () => {
-  return <UserProfile />;
+  return (
+    <MainLayout>
+      <UserProfile />
+    </MainLayout>
+  );
 };
 
 export default UserProfilePage;
