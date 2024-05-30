2c87a0b056c9df2a5a10879a4695b87aaa5cbb00
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-05-30 15:50:01 +0300

    feat: add parse product

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,18 +1,26 @@
 import { ProductProjection } from '@commercetools/platform-sdk';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { FC } from 'react';
+import parseProduct from '../../shared/utils/parseProduct';
 
 interface ProductCardProps {
   product: ProductProjection;
 }
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
-  const { masterVariant, name } = product;
+  const parsedProduct = parseProduct(product);
+
   return (
     <Card>
-      <CardMedia component="img" image={masterVariant.images?.at(0)?.url} alt={name.en} />
-      <CardContent>
-        <Typography variant="body2">{name.en}</Typography>
+      <CardMedia component="img" image={parsedProduct.images.at(0)?.url} alt={parsedProduct.name} height={300} />
+      <CardContent sx={{ textAlign: 'center' }}>
+        <Typography variant="h5" component="div">
+          {parsedProduct.name}
+        </Typography>
+        <Typography variant="caption">{parsedProduct.description}</Typography>
+        <Typography variant="h5" component="div">
+          {parsedProduct.price}
+        </Typography>
       </CardContent>
     </Card>
   );
