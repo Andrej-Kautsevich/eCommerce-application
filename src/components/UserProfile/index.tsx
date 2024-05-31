6df15972f1aeb3b19220b1b35ff19db80db99a07
Andrii Shopiak <andriyshopyak@gmail.com>
2024-05-31 11:45:34 +0300

    feat: add dipslaying first and last names to profile

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -1,10 +1,17 @@
 import { Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useCustomer } from '../../api/hooks';
 
 export default function UserProfile() {
+  const [getUserName, setUserName] = useState('');
+  const { getCustomer } = useCustomer();
+  getCustomer()
+    .then((data) => setUserName(`${data.body.firstName} ${data.body.lastName}`))
+    .catch((err: Error) => err.message);
   return (
     <Box>
       <Typography variant="h3" component="div" sx={{ textAlign: 'center' }}>
-        Welcome Andrii Candy
+        Welcome {getUserName}
       </Typography>
       <Typography variant="h6" component="div">
         Address:
