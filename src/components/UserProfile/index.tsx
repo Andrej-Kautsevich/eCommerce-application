640766b862d10b6d9243d545a598975ded51f3a0
Andrii Shopiak <andriyshopyak@gmail.com>
2024-06-01 08:26:20 +0300

    feat: add city and country displaying

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -4,9 +4,17 @@ import { useCustomer } from '../../api/hooks';
 
 export default function UserProfile() {
   const [getUserName, setUserName] = useState('');
+  const [getUserBirth, setUserBirth] = useState('');
+  const [getUserCity, setUserCity] = useState('');
+  const [getUserCountry, setUserCountry] = useState('');
   const { getCustomer } = useCustomer();
   getCustomer()
-    .then((data) => setUserName(`${data.body.firstName} ${data.body.lastName}`))
+    .then((data) => {
+      setUserName(`${data.body.firstName} ${data.body.lastName}`);
+      setUserBirth(`${data.body.dateOfBirth}`);
+      setUserCity(data.body.addresses.map((el) => `${el.city}`).join(''));
+      setUserCountry(data.body.addresses.map((el) => `${el.country}`).join(''));
+    })
     .catch((err: Error) => err.message);
   return (
     <Box>
@@ -14,8 +22,19 @@ export default function UserProfile() {
         Welcome {getUserName}
       </Typography>
       <Typography variant="h6" component="div">
-        Address:
+        Date of birth: {getUserBirth.split('-').reverse().join('-')}
       </Typography>
+      <Box>
+        <Typography variant="h6" component="div">
+          Addresses:
+        </Typography>
+        <Typography variant="h6" component="div">
+          City: {getUserCity}
+        </Typography>
+        <Typography variant="h6" component="div">
+          Country: {getUserCountry}
+        </Typography>
+      </Box>
     </Box>
   );
 }
