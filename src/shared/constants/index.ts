e5058d004c7f2ae940196c7a629e15ee6b7c8837
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-02 19:41:10 +0300

    fix: remove unused code

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -8,11 +8,3 @@ export const PRODUCT_IMAGE_PLACEHOLDER: Image[] = [
     dimensions: { h: 1000, w: 1000 },
   },
 ];
-
-export const BREADCRUMBS_NAME_MAP: { [key: string]: string } = {
-  '/': 'Inbox',
-  '/inbox/important': 'Important',
-  '/trash': 'Trash',
-  '/spam': 'Spam',
-  '/drafts': 'Drafts',
-};
