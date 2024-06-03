a574c3d9d7c9a2953cfcf5eb0ada16e8798837af
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-04 01:17:10 +0300

    fix: fix input state

--- src/components/Catalog/CatalogSearchPanel.tsx
+++ src/components/Catalog/CatalogSearchPanel.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { FormControl, IconButton, InputAdornment, OutlinedInput } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useAppDispatch } from '../../shared/store/hooks';
@@ -7,16 +7,25 @@ import { setSearchParam } from '../../shared/store/auth/productsSlice';
 const CatalogSearchPanel = () => {
   const dispatch = useAppDispatch();
 
+  const [searchInput, setSearchInput] = useState('');
+
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
+    setSearchInput(input);
     if (input.length > 3) dispatch(setSearchParam(input));
     if (!input) dispatch(setSearchParam(undefined));
   };
 
   return (
     <FormControl sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
-      <OutlinedInput margin="dense" fullWidth onChange={handleInput} endAdornment={<InputAdornment position="end" />} />
-      <IconButton disabled sx={{ p: '10px' }}>
+      <OutlinedInput
+        margin="dense"
+        fullWidth
+        onChange={handleInput}
+        value={searchInput}
+        endAdornment={<InputAdornment position="end" />}
+      />
+      <IconButton sx={{ p: '10px' }}>
         <SearchIcon />
       </IconButton>
     </FormControl>
