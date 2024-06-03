a2a530faad2fbb85a05049ee79c6e9d2a9a97e46
AndriiShopiak <90128827+AndriiShopiak@users.noreply.github.com>
2024-06-03 12:43:47 +0300

    Feature/User Profile (#44)
    * feat: add dipslaying first and last names to profile
    
    * feat: add city and country displaying
    
    * refactor: add fix for lines
    
    * refactor: disable line
    
    * refactor: fix comment
    
    * feat: add profile info about user
    
    * refactor: fix names for userAddresses state

--- src/shared/types/type.ts
+++ src/shared/types/type.ts
@@ -3,7 +3,13 @@ import { FilterCategories } from './enum';
 export type ProductKey = {
   key: string;
 };
-
+export type AddressesFields = {
+  id?: string;
+  streetName?: string;
+  postalCode?: string;
+  city?: string;
+  country?: string;
+};
 export type FilterParams = {
   [FilterCategories.CATEGORIES]?: string;
 };
