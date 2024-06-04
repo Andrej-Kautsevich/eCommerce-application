2078d0490a8405dffd8ae38c14d8b475bbc5a6ae
AndriiShopiak <90128827+AndriiShopiak@users.noreply.github.com>
2024-06-05 02:03:51 +0300

    Feature/user profile edit (#55)
    * feat: add design for editing mode
    
    * feat: add personal info validation
    
    * Update src/components/UserProfile/index.tsx
    
    * Update src/components/UserProfile/editPersonInfoForm.tsx
    
    * Update src/components/Registration/types/index.ts
    
    * fix: fix updates errors
    
    ---------
    
    Co-authored-by: Andrej Kautsevich <andrej.kautsevich@yandex.by>

---
+++ src/components/UserProfile/editPersonInfoForm.tsx
@@ -0,0 +1,69 @@
+import { Grid, Typography } from '@mui/material';
+import { TextFieldElement, useForm } from 'react-hook-form-mui';
+import { DatePickerElement } from 'react-hook-form-mui/date-pickers';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { EditPersonalInfo } from '../Registration/types';
+import schemaName from '../../shared/validation/nameValidation';
+import schemaBirthDate from '../../shared/validation/validationBirthDate';
+import schemaEmail from '../../shared/validation/emailValidation';
+
+export default function EditInfo() {
+  const schema = yup.object().shape({
+    firstName: schemaName,
+    lastName: schemaName.required('Last name is required'),
+    dateOfBirth: schemaBirthDate,
+    email: schemaEmail,
+  });
+  const { control } = useForm<EditPersonalInfo>({ mode: 'onChange', resolver: yupResolver(schema) });
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <TextFieldElement
+          name="firstName"
+          required
+          fullWidth
+          id="firstName"
+          label="First Name"
+          autoFocus
+          autoComplete="given-name"
+          control={control}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextFieldElement
+          required
+          fullWidth
+          id="lastName"
+          label="Last Name"
+          name="lastName"
+          autoComplete="family-name"
+          control={control}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <DatePickerElement
+          inputProps={{ fullWidth: true, autoComplete: 'bday', id: 'date' }}
+          required
+          helperText="You must be at least 18 years old to visit site"
+          name="dateOfBirth"
+          control={control}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="div" sx={{ mb: 1 }}>
+          Email & Password
+        </Typography>
+        <TextFieldElement
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          control={control}
+        />
+      </Grid>
+    </Grid>
+  );
+}
--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useCustomer } from '../../api/hooks';
 import { AddressesFields } from '../../shared/types/type';
+import EditInfo from './editPersonInfoForm';
 
 export default function UserProfile() {
   const [getUserName, setUserName] = useState('');
@@ -9,6 +10,8 @@ export default function UserProfile() {
   const [getUserBirth, setUserBirth] = useState('');
   const [getUserDefaultShip, setUserDefaultShip] = useState('');
   const [userAddresses, setUserAddresses] = useState([{}]);
+  const [userEmail, setUserEmail] = useState('');
+  const [showEditMode, setShowEditMode] = useState(false);
   const { getCustomer } = useCustomer();
 
   useEffect(() => {
@@ -19,26 +22,83 @@ export default function UserProfile() {
       setUserBirth(`${response.dateOfBirth}`);
       setUserLastName(`${response.lastName}`);
       setUserDefaultShip(`${response.defaultShippingAddressId}`);
+      setUserEmail(`${response.email}`);
     };
     getUserData().catch((err: Error) => err);
   }, [getCustomer]);
   return (
     <Box sx={{ paddingTop: '50px' }}>
       <Typography variant="h3" component="div" sx={{ textAlign: 'center' }}>
-        {getUserName} {getUserLastName}
+        Hey, {getUserName} {getUserLastName}
       </Typography>
-      <Typography variant="h6" component="div" sx={{ textAlign: 'center' }}>
-        Date of birth: {getUserBirth.split('-').reverse().join('-')}
+      <Typography
+        variant="h6"
+        component="p"
+        sx={{ textAlign: 'center', fontSize: '14px', fontWeight: '300', color: '#939393', margin: '21px 0' }}
+      >
+        Welcome to your profile, your one-stop-shop for all your recent Volcano Watch account activity.
       </Typography>
-      <Typography variant="h4" component="div" sx={{ textAlign: 'center', marginTop: '10px' }}>
+      {showEditMode ? (
+        <Box
+          component="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            // console.log('Send data');
+          }}
+        >
+          <Typography variant="h5" component="div" sx={{ mb: 1 }}>
+            Please type your new data
+          </Typography>
+          <EditInfo />
+          <Box sx={{ display: 'flex', gap: '10px', flexWrap: 'wrap', mt: 1 }}>
+            <Button type="submit" variant="contained">
+              Save Changes
+            </Button>
+            <Button variant="contained" onClick={() => setShowEditMode(false)}>
+              Cancel
+            </Button>
+          </Box>
+        </Box>
+      ) : (
+        <Box sx={{ border: '2px solid #eaecf5', borderRadius: '10px', p: 3 }}>
+          <Typography variant="h6" component="div" sx={{ color: '#939393' }}>
+            My info
+          </Typography>
+          <Typography variant="h6" component="div">
+            Name: {getUserName} {getUserLastName}
+          </Typography>
+          <Typography variant="h6" component="div">
+            Date of birth: {getUserBirth.split('-').reverse().join('-')}
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ color: '#939393', mt: 1 }}>
+            Email & Password
+          </Typography>
+          <Typography variant="h6" component="div">
+            Email: {userEmail}
+          </Typography>
+          <Typography variant="h6" component="div">
+            Password: *******
+          </Typography>
+          <Button variant="contained" onClick={() => setShowEditMode(true)}>
+            Manage Info
+          </Button>
+        </Box>
+      )}
+      <Typography variant="h5" component="div" sx={{ marginTop: '50px' }}>
         Addresses ({userAddresses.length}):
       </Typography>
       <Box
-        sx={{ fontFamily: 'Poppins', display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', gap: '10px' }}
+        sx={{
+          fontFamily: 'Poppins',
+          display: 'flex',
+          justifyContent: 'space-around',
+          flexDirection: 'column',
+          gap: '10px',
+        }}
       >
         {userAddresses.map((info: AddressesFields) => (
-          <Box>
-            <Typography variant="h6" component="div" sx={{ bgcolor: 'primary.main', color: 'white', pr: 3, pl: 3 }}>
+          <Box sx={{ border: '2px solid #eaecf5', borderRadius: '10px', p: 3 }}>
+            <Typography variant="h6" component="div" sx={{ color: '#939393' }}>
               {getUserDefaultShip === info.id ? 'Default shipping:' : 'Default billing:'}
             </Typography>
             <Typography variant="h6" component="div">
@@ -53,6 +113,7 @@ export default function UserProfile() {
             <Typography variant="h6" component="div">
               Postal Code: {info.postalCode}
             </Typography>
+            <Button variant="contained">Manage Info</Button>
           </Box>
         ))}
       </Box>
