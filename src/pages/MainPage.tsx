71cbff8ed1007011120656ea733f10f5be538016
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-06 01:11:38 +0300

    feat: add image to main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { Alert, AlertTitle, Slide, Snackbar, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { useAppDispatch, useAppSelector } from '../shared/store/hooks';
 import { setSubmitSuccess } from '../shared/store/auth/authSlice';
 import MainLayout from '../shared/ui/MainLayout';
+import mainImageBackground from '../shared/assets/images/backgroundMainPage.jpg';
 
 const MainPage = () => {
   const { submitSuccess } = useAppSelector((state) => state.auth);
@@ -19,9 +21,30 @@ const MainPage = () => {
 
   return (
     <MainLayout>
-      <Typography gutterBottom variant="h2" component="h1" fontFamily="Orbitron" width="50vw" sx={{ pt: 15, pl: 8 }}>
-        Find your dream watch
-      </Typography>
+      <Grid container sx={{ padding: 0 }}>
+        <Grid xs={12} sm={6}>
+          <Typography
+            gutterBottom
+            variant="h2"
+            component="h1"
+            fontFamily="Orbitron"
+            width="100%"
+            sx={{ pt: 15, pl: 8 }}
+          >
+            Find your dream watch
+          </Typography>
+        </Grid>
+        <Grid
+          minHeight={200}
+          xs={12}
+          sm={6}
+          sx={{
+            backgroundImage: `url(${mainImageBackground})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        />
+      </Grid>
 
       {submitSuccess.status && (
         <Slide in={openSnackBar} direction="right">
