55d7cf51e3efc940d4654cbe01470e41a542df16
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-12 21:58:24 +0300

    feat: add page title

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,7 +1,17 @@
+import { Typography } from '@mui/material';
+import PageTitle from '../components/PageTitle';
 import MainLayout from '../shared/ui/MainLayout';
 
 const BasketPage = () => {
-  return <MainLayout>Basket Page</MainLayout>;
+  return (
+    <MainLayout>
+      <PageTitle>
+        <Typography variant="h3" component="h1" fontFamily="Orbitron" color="secondary">
+          Your Cart
+        </Typography>
+      </PageTitle>
+    </MainLayout>
+  );
 };
 
 export default BasketPage;
