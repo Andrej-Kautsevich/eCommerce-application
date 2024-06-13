9a68b819ef60ec7a5a3dd24b57162765da829f89
Artyom Tychshuk <46044804+Ympakaloky@users.noreply.github.com>
2024-06-13 21:09:46 +0500

    Feature/create cart (#65)
    * feat: cart creation
    
    * feat: cart creation
    
    * feat: addProduct & deleteProduct

--- src/components/AddCartBtn/index.tsx
+++ src/components/AddCartBtn/index.tsx
@@ -1,9 +1,41 @@
+import { useLocation } from 'react-router-dom';
 import { ShoppingCartOutlined } from '@mui/icons-material';
 import { Button } from '@mui/material';
+import { useCustomer } from '../../api/hooks';
+import useCart from '../../api/hooks/useCart';
+import { useAppDispatch } from '../../shared/store/hooks';
+import { setProductList, setTotalProducts } from '../../shared/store/auth/cartSlice';
 
 export default function AddCartBtn() {
+  const { getCart } = useCustomer();
+  const { addItem } = useCart();
+  const dispatch = useAppDispatch();
+  const productID = useLocation().pathname.split('/').slice(2).join();
+
+  const addProduct = (productId: string) => async () => {
+    try {
+      const response = await getCart();
+      const fetchAddItem = async () => {
+        await addItem(response.body.results[0].version, productId);
+        const updatedCart = await getCart();
+        dispatch(setTotalProducts(updatedCart.body.results[0].totalLineItemQuantity));
+
+        const itemList = updatedCart.body.results[0].lineItems.map((item) => ({
+          id: item.id,
+          quantity: item.quantity,
+        }));
+        dispatch(setProductList(itemList));
+      };
+      // eslint-disable-next-line no-console
+      fetchAddItem().catch((error) => console.log(error));
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error adding item to cart:', error);
+    }
+  };
+
   return (
-    <Button variant="contained">
+    <Button variant="contained" onClick={addProduct(productID)}>
       Add to Cart
       <ShoppingCartOutlined fontSize="large" sx={{ color: 'primary.contrastText', ml: 1 }} />
     </Button>
