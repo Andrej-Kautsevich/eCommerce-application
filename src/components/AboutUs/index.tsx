6ecf11f92e91264affc80c2e3bb7e055b30f0aee
Artyom (Ympakaloky) <Ympakaloky@gmail.com>
2024-06-13 22:18:17 +0500

    feat: add about us page and cards

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -4,55 +4,74 @@ import MainLayout from '../../shared/ui/MainLayout';
 import PageTitle from '../PageTitle';
 import CatalogBreadcrumbs from '../CatalogBreadcrumbs';
 import { TeamMemberCard } from '../../shared/types/type';
-import teamMembers from './teamMembers';
+import { teamMembers, introduction, mainText, finalText } from './teamMembers';
 import emptyImage from '../../shared/assets/images/empty-img.png';
+import responsiveTheme from '../../shared/ui/theme';
 
 const Photo = ({ image }: { image: string }) => {
   if (!image) return null;
   return (
-    <CardMedia
-      component="img"
-      image={image}
-      alt={image ?? 'Image'}
-      onError={(e) => {
-        const target = e.target as HTMLImageElement;
-        target.onerror = null;
-        target.src = emptyImage;
-      }}
-      style={{
-        width: 'auto',
-        height: 'auto',
-        objectFit: 'cover',
-        display: 'block',
-        margin: 'auto',
-      }}
-    />
+    <Box sx={{ width: 200, height: 200 }}>
+      <CardMedia
+        component="img"
+        image={image}
+        alt={image ?? 'Image'}
+        onError={(e) => {
+          const target = e.target as HTMLImageElement;
+          target.onerror = null;
+          target.src = emptyImage;
+        }}
+        style={{
+          width: 'auto',
+          height: 'auto',
+          objectFit: 'cover',
+          display: 'block',
+          margin: 'auto',
+        }}
+      />
+    </Box>
   );
 };
 
 const TeamMemberCardComponent = ({ name, role, bio, photo, profile }: TeamMemberCard) => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Photo image={photo} />
-        <Typography variant="h5">{name}</Typography>
-        <Link href={profile} underline="hover">
-          {profile.split('/').slice(3).join()}
-        </Link>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderRadius: '12px',
+        boxShadow: `0px 4px 8px ${responsiveTheme.palette.primary.main}`,
+        padding: 3,
+        mt: 2,
+        height: '100%',
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Photo image={photo} />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <Typography variant="h5">{name}</Typography>
+          <Link href={profile} underline="hover">
+            GitHub {/* profile.split('/').slice(3).join() */}
+          </Link>
+          <Typography variant="h6" component="h6" fontFamily="Orbitron">
+            {role}
+          </Typography>
+        </Box>
       </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Typography variant="subtitle1">{role}</Typography>
-        <Typography>{bio}</Typography>
-      </Box>
+
+      <Typography align="justify">{bio}</Typography>
     </Box>
   );
 };
 
 const TeamCards = () => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} columns={{ xs: 1, sm: 1, md: 2 }}>
       {teamMembers.map((member) => (
-        <Grid item key={member.name}>
+        <Grid item key={member.name} xs={1} sm={1} md={1}>
           <TeamMemberCardComponent
             name={member.name}
             role={member.role}
@@ -77,7 +96,16 @@ const AboutUs = () => {
           <CatalogBreadcrumbs />
         </Box>
       </PageTitle>
+      <Typography variant="body1" component="p" align="justify" sx={{ mb: 2, mt: 3 }}>
+        {introduction}
+      </Typography>
       <TeamCards />
+      <Typography variant="body1" component="p" align="justify" sx={{ mb: 3, mt: 5 }}>
+        {mainText}
+      </Typography>
+      <Typography variant="body1" component="p" align="justify" sx={{ mb: 5, mt: 2 }}>
+        {finalText}
+      </Typography>
     </MainLayout>
   );
 };
--- src/components/AboutUs/teamMembers.ts
+++ src/components/AboutUs/teamMembers.ts
@@ -1,34 +1,37 @@
 import { TeamMemberCard } from '../../shared/types/type';
 
 // TODO fill in the fields
-const teamMembers: TeamMemberCard[] = [
+export const teamMembers: TeamMemberCard[] = [
   {
     name: 'Yuliya Niaverava',
     role: 'Mentor',
-    bio: 'some text',
+    bio: `Expressed great pride in the team's achievements. She noted our high level of professionalism, teamwork and dedication to the project. Julia emphasized the team's ability to overcome difficulties and create a product that exceeded all expectations.`,
     photo: 'https://rs.school/assets/rs-school-CZS_yQWd.webp',
     profile: 'https://github.com/YuliyaMinsk',
   },
   {
     name: 'Andrej Kautsevich',
     role: 'Team leader',
-    bio: 'some text',
+    bio: `Andrej demonstrated strong leadership qualities, effectively coordinating the work of the team. He successfully organized the development process, distributed tasks and ensured their timely completion. His technical skills and ability to solve complex problems were key factors in the success of the project. Andrey also set up a GitHub repository, which ensured convenient project management, and implemented Redux technology, which significantly improved application state management.`,
     photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj4DMW4yKu0S-n74moTrsHd6r8FFBZ_4F6hQ&s',
     profile: 'https://github.com/Andrej-Kautsevich',
   },
   {
     name: 'Andrii Shopiak',
-    role: 'junior frontend developer',
-    bio: 'some text',
+    role: 'Junior frontend developer',
+    bio: `Andreii demonstrated a deep understanding of frontend technologies and a commitment to excellence. He implemented modern development techniques, which significantly improved the performance and stability of the application. His attention to detail and code quality was top notch. In addition, Andreii developed a registration and validation system, which ensured the security and ease of use of the application.`,
     photo: 'https://rs.school/assets/mentors-wanted-DNAQKC3G.webp',
     profile: 'https://github.com/AndriiShopiak',
   },
   {
     name: 'Artyom Tychshuk',
-    role: 'junior frontend developer',
-    bio: 'some text',
+    role: 'Junior frontend developer',
+    bio: `Artyom has proven himself to be a creative and purposeful developer. He played a key role in creating a modern and user-friendly user interface. His understanding of design principles and their practical application greatly enhanced the visual appeal of the project. In addition, Artyom developed a detailed product page and customized eCommerceTools, which significantly improved the functionality and usability of the store.`,
     photo: 'https://rs.school/assets/speakers-wanted-D335Oh-p.webp',
     profile: 'https://github.com/Ympakaloky',
   },
 ];
-export default teamMembers;
+
+export const introduction = `Our team of three aspiring frontend developers who successfully completed their final project - an online watch store - as part of their training at RSSchool. Their work was carried out under the guidance of mentor ${teamMembers[0].name}, who provided invaluable assistance and support at all stages of development.`;
+export const mainText = `Our team effectively used Agile development principles, which contributed to the flexibility and efficiency of project execution. Regular stand-ups and open communication allowed the team to promptly identify and solve emerging problems. Each team member made his own unique contribution, which led to the creation of a high-quality and functional online watch store.`;
+export const finalText = `The team has demonstrated its ability to handle complex challenges and achieve impressive results. Our successful collaboration and technical competence highlight our potential for future advancements in the field of web development. Working under the guidance of a mentor, we not only developed a wonderful application, but also laid a solid foundation for our professional growth.`;
