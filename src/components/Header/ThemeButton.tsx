eaa6b6d5455a4c0ab878e7d9a48ee14468ff0bbf
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-16 22:46:50 +0300

    feat: add icon change

--- src/components/Header/ThemeButton.tsx
+++ src/components/Header/ThemeButton.tsx
@@ -1,19 +1,15 @@
 import { IconButton } from '@mui/material';
-import { Brightness7 } from '@mui/icons-material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
 import useColorMode from '../../shared/utils/theme/useColorMode';
 
 const ThemeButton = () => {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, mode } = useColorMode();
 
   const handleThemeToggle = () => {
     toggleColorMode();
   };
 
-  return (
-    <IconButton onClick={handleThemeToggle}>
-      <Brightness7 />
-    </IconButton>
-  );
+  return <IconButton onClick={handleThemeToggle}>{mode === 'dark' ? <Brightness7 /> : <Brightness4 />}</IconButton>;
 };
 
 export default ThemeButton;
