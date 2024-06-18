277e84d8a7f255851ef679aeee00ca8974b20325
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-06-18 11:11:46 +0300

    feat: add loading state to product card buttons

--- src/components/AddCartBtn/index.tsx
+++ src/components/AddCartBtn/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { ShoppingCartOutlined } from '@mui/icons-material';
-import { Box, Button } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Box, styled } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import { useTranslation } from 'react-i18next';
 import useCart from '../../api/hooks/useCart';
@@ -8,31 +10,50 @@ import { ProductIDCartBtnProps } from '../../shared/types/interface';
 import { SnackbarMessages } from '../../shared/types/enum';
 import getSnackbarMessage from '../../shared/utils/getSnackbarMessage';
 
+const StyledLoadingButton = styled(LoadingButton)(({ theme }) => ({
+  [theme.breakpoints.down('xs')]: {
+    size: 'small',
+  },
+  [theme.breakpoints.up('sm')]: {
+    size: 'large',
+  },
+}));
+
 function AddCartBtn({ productID }: ProductIDCartBtnProps) {
   const { t } = useTranslation();
   const { addItem } = useCart();
   const cart = useAppSelector((state) => state.cart.cart);
   const { fetchCart } = useCart();
   const { enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
 
   const addProduct = () => async () => {
     if (cart) {
       try {
+        setLoading(true);
         await addItem(cart.version, productID!);
         await fetchCart();
         enqueueSnackbar(getSnackbarMessage(SnackbarMessages.ADD_ITEM_SUCCESS, t), { variant: 'success' });
+        setLoading(false);
       } catch (error) {
         enqueueSnackbar(getSnackbarMessage(SnackbarMessages.ADD_ITEM_FETCH_ERROR, t), { variant: 'error' });
+        setLoading(false);
       }
     }
   };
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', mb: 3 }}>
-      <Button variant="contained" onClick={addProduct()} sx={{ height: '50px', width: 200 }}>
-        {t('Add to Cart')}
-        <ShoppingCartOutlined fontSize="large" sx={{ color: 'primary.contrastText', ml: 1 }} />
-      </Button>
+      <StyledLoadingButton
+        loading={loading}
+        fullWidth
+        variant="contained"
+        onClick={addProduct()}
+        loadingPosition="end"
+        endIcon={<ShoppingCartOutlined fontSize="large" sx={{ color: 'primary.contrastText' }} />}
+      >
+        <span>{t('Add to Cart')}</span>
+      </StyledLoadingButton>
     </Box>
   );
 }
--- src/components/DeleteCartBtn/index.tsx
+++ src/components/DeleteCartBtn/index.tsx
@@ -1,5 +1,6 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
 import { useSnackbar } from 'notistack';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { useTranslation } from 'react-i18next';
 import useCart from '../../api/hooks/useCart';
 import { useAppSelector } from '../../shared/store/hooks';
@@ -14,33 +15,38 @@ function DeleteCartBtn({ itemID, quantity }: ProductIDCartBtnProps) {
   const { fetchCart } = useCart();
   const lessQuantity = quantity! - 1;
   const { enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
 
   const deleteProduct = () => async () => {
     if (cart) {
       try {
+        setLoading(true);
         if (quantity! > 1) {
           await changeItemQuantity(cart.version, itemID!, lessQuantity);
         } else {
           await deleteItem(cart.version, itemID!);
         }
+        setLoading(false);
         enqueueSnackbar(getSnackbarMessage(SnackbarMessages.REMOVE_ITEM_SUCCESS, t), { variant: 'success' });
 
         await fetchCart();
       } catch (error) {
         enqueueSnackbar(getSnackbarMessage(SnackbarMessages.DELETE_ITEM_FETCH_ERROR, t), { variant: 'error' });
+        setLoading(false);
       }
     }
   };
 
   return (
-    <Button
+    <LoadingButton
       variant="contained"
+      loading={loading}
       onClick={deleteProduct()}
       disabled={quantity! < 1}
       sx={{ mb: 3, height: '50px', width: 200 }}
     >
-      {t('Remove from Cart')}
-    </Button>
+      <span>{t('Remove from Cart')}</span>
+    </LoadingButton>
   );
 }
 
